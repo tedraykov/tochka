7a7c30f7f3459d5e18dc8d4da24ac2aa74099c01
Teodor Raykov <teodorr@teodorr-a01.vmware.com>
2019-10-09 12:09:50 +0300

    Sorting users by their points

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UsersService, User } from 'src/app/users.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfigureUserComponent } from '../configure-user/configure-user.component';
@@ -14,7 +15,9 @@ export class UserListComponent implements OnInit {
   constructor(private userService: UsersService, private dialog: MatDialog) { }
 
   ngOnInit() {
-    this.users = this.userService.getAllUsers$();
+    this.users = this.userService.getAllUsers$().pipe(
+      map((users: User[]) => users.sort((a, b) => b.points - a.points))
+    );
   }
 
   addUserDialog() {
